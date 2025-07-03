2eee426e47d009fcd3d7dfa8111792f059239ac9
Hhmm0213 <147248677+Hhmm0213@users.noreply.github.com>
2025-07-04 00:39:11 +0900

    Update Outer_Interrupt.h

--- Spresense_Interrupt_MultiCore/src/Outer_Interrupt.h
+++ Spresense_Interrupt_MultiCore/src/Outer_Interrupt.h
@@ -1,1 +1,14 @@
+#ifndef OUTER_INTERRUPT_H
+#define  OUTER_INTERRUPT_H
+// 割り込み発生を通知するためのフラグ
+volatile bool interrupt_fired = false;
 
+// ----------------------------------------------------
+// メインコアで実行される割り込みハンドラ (ISR)
+// ----------------------------------------------------
+void main_core_isr() {
+  interrupt_fired = true;
+}
+
+
+#endif //OUTER_INTERRUPT_H
